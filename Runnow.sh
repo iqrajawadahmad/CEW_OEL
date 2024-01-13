0639de0f54c7e55f72226005645c5d504baeccf8
maazaslam786 <71929760+maazaslam786@users.noreply.github.com>
2024-01-13 22:50:38 +0500

    Add files via upload
    Commiting 3 files

---
+++ Runnow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Compile the C program
+gcc -c fetch_data.c -o fetchtemp.o
+gcc -c visibilty_checks.c -o visibiltytemp.o 
+gcc -c main.c -o maintemp.o
+gcc fetchtemp.o  visibiltytemp.o maintemp.o  -o compiledweather -lcurl -ljson-c
+
+# Run the compiled program
+./compiledweather
+
+# Optionally, clean up the compiled object files
+rm fetchtemp.o visibiltytemp.o maintemp.o
+
+(crontab -l ; echo "0 */3 * * * $(pwd)/run_program.sh") | crontab -
+
